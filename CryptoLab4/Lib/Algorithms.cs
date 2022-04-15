6efa5309ac6a497557116ab4eb1447c4b5ef7e5e
Shimazu <mutigullin2000@gmail.com>
2022-04-15 18:39:55 +0300

    another one

---
+++ CryptoLab4/Lib/Algorithms.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+using System.Diagnostics;
+
+namespace CryptoLab4.Lib
+{
+    public static class Algorithms
+    {
+        /// <summary>
+        /// Returns Reatest Commaon Divisor of a and b
+        /// </summary>
+        public static BigInteger EuclideanAlgorithm(BigInteger a, BigInteger b)
+        {
+            while (b != 0)
+            {
+                BigInteger temp = a % b;
+                a = b;
+                b = temp;
+            }
+            return a;
+        }
+
+        public static BigInteger FastExponentiationAlgorithm(BigInteger basis, BigInteger power, BigInteger mod)
+        {
+            BigInteger result = 1;
+            while (power > 0)
+            {
+                if (!power.IsEven)
+                    result = (result * basis) % mod;
+                power /= 2;
+                basis = (basis * basis) % mod;
+            }
+            return result;
+        }
+
+        public static (BigInteger, BigInteger, BigInteger) ExtendedEuclideanAlgorithm(BigInteger a, BigInteger b)
+        {
+            BigInteger u1 = 1, u2 = 0, v1 = 0, v2 = 1;
+            while (b != 0)
+            {
+                BigInteger q = a / b;
+                BigInteger r = a % b;
+                a = b;
+                b = r;
+                r = u2;
+                u2 = u1 - q * u2;
+                u1 = r;
+                r = v2;
+                v2 = v1 - q * v2;
+                v1 = r;
+            }
+            return (a, u1, v1);
+        }
+
+        public static BigInteger GetSimpleRandomNumber(int lengthInBits)
+        {
+            int lengthInBytes = lengthInBits / 8;
+            int k = 10;
+            Random random = new Random();
+            byte[] simpleRandomInBytes = new byte[lengthInBytes + 1];
+            byte[] rndInBytes = new byte[lengthInBytes + 1];
+            BigInteger rnd;
+            BigInteger simpleRandom = 0;
+
+            for (int i = 0; i < k;)
+            {
+                i = 0;
+                random.NextBytes(simpleRandomInBytes);
+                simpleRandomInBytes[lengthInBytes] = (byte)(simpleRandomInBytes[lengthInBytes] & 127);
+                simpleRandomInBytes[lengthInBytes] = (byte)(simpleRandomInBytes[lengthInBytes] | 64);
+                simpleRandom = new BigInteger(simpleRandomInBytes) >> 7;
+
+                while (i < k)
+                {
+                    random.NextBytes(rndInBytes);
+                    rndInBytes[lengthInBytes] = (byte)(rndInBytes[lengthInBytes] & 63);
+                    rnd = (new BigInteger(rndInBytes) >> 7) + 1;
+
+                    if (FastExponentiationAlgorithm(rnd, simpleRandom - 1, simpleRandom) == 1)
+                        i++;
+                    else
+                        break;
+
+                }
+            }
+            return simpleRandom;
+        }
+
+        /// <summary>
+        /// Returns one of dividers of n
+        /// </summary>
+        public static BigInteger PollardAlgorithm(BigInteger n)
+        {
+            List<BigInteger> x = new List<BigInteger> { GetSimpleRandomNumber((int)n.GetBitLength()-1),
+                                                        GetSimpleRandomNumber((int)n.GetBitLength()-1)};
+            while (true)
+            {
+                if (((BigInteger)x.Count).IsPowerOfTwo)
+                    for (int i = 1; i < x.Count; i++)
+                    {
+                        BigInteger gcd = EuclideanAlgorithm(n, BigInteger.Abs(x.Last() - x[i]));
+                        if (gcd != n && gcd > 1)
+                            return gcd;
+                    }
+                x.Add((x.Last() * x.Last() + 1) % n);
+            }
+        }
+
+
+    }
+}
---
+++ CryptoLab4/Lib/MyRSA.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoLab4.Lib
+{
+    public class MyRSA
+    {
+        int KeySize;
+        int OpenExponent;
+        BigInteger P;
+        BigInteger Q;
+
+        BigInteger EulerFunction() => (P-1)*(Q-1);
+        BigInteger Module() => P*Q;
+        BigInteger ClosedExponent() => Algorithms.ExtendedEuclideanAlgorithm(OpenExponent, EulerFunction()).Item2;
+
+        public MyRSA(int keySize = 1024, int openExponent = 65537)
+        {
+            KeySize = keySize;
+            OpenExponent = openExponent;
+            P = Algorithms.GetSimpleRandomNumber(KeySize / 2);
+            Q = Algorithms.GetSimpleRandomNumber(KeySize / 2);
+        }
+      
+        public MyRSA(BigInteger p, BigInteger q, int openExponent = 65537)
+        {
+            P = p;
+            Q = q;
+            OpenExponent = openExponent;
+            KeySize = (int)(P.GetBitLength() + Q.GetBitLength());
+        }
+
+        public (int, BigInteger) GetPublicKey() => (OpenExponent, Module());
+        public (BigInteger, BigInteger) GetPrivateKey() => (ClosedExponent(), Module());
+        public void Encrypt(string fileName) => Encrypt(fileName, (OpenExponent, Module()));
+        public string[] Encrypt(byte[] bytes) => Encrypt(bytes, (OpenExponent, Module()));
+        public void Decrypt(string fileName) => Decrypt(fileName, (ClosedExponent(), Module()));
+        public byte[] Decrypt(string[] encryptedText) => Decrypt(encryptedText, (OpenExponent, Module()));
+
+
+        public static void Encrypt(string fileName, (int, BigInteger) publicKey)
+        {
+            byte[] bytes = Converter.FileToBytes(fileName);
+            string[] encryptedText = Encrypt(bytes, publicKey);
+            StreamWriter sw = new StreamWriter("encrypted.txt");
+            foreach (string line in encryptedText)
+                sw.WriteLine(line);
+            sw.Close();
+        }
+
+        public static string[] Encrypt(byte[] bytes, (int, BigInteger) publicKey)
+        {
+            int blocksLengthInBytes = (int)publicKey.Item2.GetBitLength() / 4 / 8;
+            int blocksCount = bytes.Length / blocksLengthInBytes;
+            string[] encryptedText = new string[blocksCount];
+            for (int i = 0; i < blocksCount; i++)
+            {
+                byte[] blockInBytes = new byte[blocksLengthInBytes];
+                Array.Copy(bytes, i * blocksLengthInBytes, blockInBytes, 0, blocksLengthInBytes);
+                if ((blockInBytes[blockInBytes.Length - 1] & 0x80) > 0)
+                {
+                    byte[] temp = new byte[blockInBytes.Length];
+                    Array.Copy(blockInBytes, temp, blockInBytes.Length);
+                    blockInBytes = new byte[temp.Length + 1];
+                    Array.Copy(temp, blockInBytes, temp.Length);
+                }
+                BigInteger block = new BigInteger(blockInBytes);
+                encryptedText[i] = Algorithms.FastExponentiationAlgorithm(block, publicKey.Item1, publicKey.Item2).ToString();
+            }
+            return encryptedText;
+        }
+
+        public static void Decrypt(string fileName, (BigInteger, BigInteger) privateKey)
+        {
+            string[] encryptedText = File.ReadAllLines(fileName);
+            byte[] decryptedBytes = Decrypt(encryptedText, privateKey);
+            using FileStream fsNew = new FileStream("decrypted.txt", FileMode.Create, FileAccess.Write);
+            fsNew.Write(decryptedBytes, 0, decryptedBytes.Length);
+        }
+
+        public static byte[] Decrypt(string[] encryptedText, (BigInteger, BigInteger) privateKey)
+        {
+            int blocksLengthInBytes = (int)privateKey.Item2.GetBitLength() / 4 / 8;
+            byte[] decryptedBytes = new byte[blocksLengthInBytes * encryptedText.Length];
+            BigInteger block;
+            for (int i = 0; i < encryptedText.Length; i++)
+            {
+                block = BigInteger.Parse(encryptedText[i]);
+                byte[] decryptedBlock = Algorithms.FastExponentiationAlgorithm(block, privateKey.Item1, privateKey.Item2).ToByteArray();
+                try
+                {
+                    Array.Copy(decryptedBlock, 0, decryptedBytes, i * blocksLengthInBytes, blocksLengthInBytes);
+                }
+                catch (Exception)
+                {
+                    continue;
+                }
+            }
+            return decryptedBytes; 
+        }
+    }
+}
